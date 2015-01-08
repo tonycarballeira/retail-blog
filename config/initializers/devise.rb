4f9988ec2df8b42c78fe7db7de3b59a99d1d5af0
tonycarballeira <tonycarballeira@gmail.com>
2015-01-08 16:34:15 -0500

    Added User Links to Views and modified styling

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -227,7 +227,7 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -256,4 +256,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
 end
