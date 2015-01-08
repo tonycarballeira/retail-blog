8baa565881787bbd130e3040657b4af1e617ac00
tonycarballeira <tonycarballeira@gmail.com>
2015-01-08 17:24:06 -0500

    Modified Routes and Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get "register", to: 'devise/registrations#new', as: :register
+    get "login", to: 'devise/sessions#new', as: :login
+    get "logout", to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+    get 'feed', to: 'statuses#index', as: :feed
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,6 +16,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'statuses#index'
    
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
