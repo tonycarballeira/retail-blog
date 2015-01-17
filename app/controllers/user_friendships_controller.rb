2b51d001249acb54a4f56402b2a6e3e0d7370fec
tonycarballeira <tonycarballeira@gmail.com>
2015-01-17 17:21:46 -0500

    Modified UserFriendships Controller

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,6 +1,7 @@
 class UserFriendshipsController < ApplicationController
 	before_action :authenticate_user!, only: [:new]
 	def new
-
+		unless params[:friend_id]
+			flash[:error] = "Friend Required"
 	end
 end
