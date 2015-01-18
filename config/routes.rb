da9d146b356b33dd588f705572752cc6f5dc3642
tonycarballeira <tonycarballeira@gmail.com>
2015-01-18 15:12:29 -0500

    Added Link To Add Friendship On Profile Show View

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
+  
 
   get 'profile/show'
 
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'statuses#index'
    
-   get '/:id', to: 'profiles#show', as: 'profile'
+   get '/:id', to: 'profiles#show'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
